df40101b4a73159ae2da4db00661b96d3f636d6b
borat <boram8655@gmail.com>
2023-12-11 16:51:14 -0300

    First commit with Nav bar done

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="tv" element={<Tv />}></Route>
+        <Route path="search" element={<Search />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
