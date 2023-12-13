cf0282fc02ea49f93b4fb4b7251364cbf7f80770
borat <boram8655@gmail.com>
2023-12-13 17:06:25 -0300

    Receiving movie infos, Creating Home of Netflex

---
+++ src/api.ts
@@ -0,0 +1,30 @@
+const API_KEY = "5875916d01b46024d4e80220b502cb79";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  backdrop_path: string;
+  id: number;
+  original_language: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  vote_average: number;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
